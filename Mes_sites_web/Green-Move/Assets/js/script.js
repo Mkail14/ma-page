5aa82ca8ba7623893f9961328775c27c38065fcb
Aloe VERA <aloe.vera@aloalo.alomayjob.com>
2026-05-11 13:24:45 +0300

    Green move

---
+++ Mes_sites_web/Green-Move/Assets/js/script.js
@@ -0,0 +1,10 @@
+const tarifButtons = document.querySelectorAll("#tarifs .tarif-btn");
+
+            tarifButtons.forEach((button) => {
+                button.addEventListener("click", (event) => {
+                    event.preventDefault();
+
+                    tarifButtons.forEach((btn) => btn.classList.remove("is-selected"));
+                    button.classList.add("is-selected");
+                });
+            });
